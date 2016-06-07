f5e6ed4300862130c8a3c59671dabfefd7f2da29
xugang <xugang0409@163.com>
2016-06-07 23:36:58 +0800

    swig-test

---
+++ swig-test/routes/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', {
+    title: 'Express',
+    items:[{
+      name:'item1'
+    },{
+      name:'item2'
+    },{
+      name:'item3'
+    }]
+  });
+});
+
+module.exports = router;
