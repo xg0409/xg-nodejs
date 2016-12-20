3075dbd1224d501e07b28b0278938a1ccd91f137
xugang <xugang0409@163.com>
2016-12-20 22:29:06 +0800

    update

--- express-hbs-test/app.js
+++ express-hbs-test/app.js
@@ -8,6 +8,15 @@ var bodyParser = require('body-parser');
 var exphbs = require('express-handlebars');
 var app = express();
 
+
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 var handlebars = exphbs.create({
   defaultLayout: 'layout',
   extname: '.hbs',
@@ -20,50 +29,27 @@ app.engine('.hbs', handlebars.engine);
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, 'views'));
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/api', require('./routes/api'));
 app.use('/', require('./routes/pages'));
 
 
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   var err = new Error('Not Found');
-//   err.status = 404;
-//   next(err);
-// });
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
-// // error handlers
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    error: err
+  });
+});
 
-// // development error handler
-// // will print stacktrace
-// if (app.get('env') === 'development') {
-//   app.use(function(err, req, res, next) {
-//     res.status(err.status || 500);
-//     res.render('error', {
-//       message: err.message,
-//       error: err
-//     });
-//   });
-// }
-
-// // production error handler
-// // no stacktraces leaked to user
-// app.use(function(err, req, res, next) {
-//   res.status(err.status || 500);
-//   res.render('error', {
-//     message: err.message,
-//     error: {}
-//   });
-// });
-
-app.listen(3000,function(){
+app.listen(3000, function() {
   console.log('To start the 3000 service')
 })
 
